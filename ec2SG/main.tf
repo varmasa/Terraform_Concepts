7e1f33120dc4c0cb8a4d50a9c40f9f9766eddb26
root <root@ip-172-31-10-28.ap-south-1.compute.internal>
2026-07-04 11:29:50 +0000

    Files added

---
+++ ec2SG/main.tf
@@ -0,0 +1,57 @@
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+# Get a subnet from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Create Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0d351f1b760a30161" # Replace with your AMI ID
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
