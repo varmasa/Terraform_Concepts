a7b8d5a829b3bd12f7f86a6d410a76b1086e0b86
root <root@ip-172-31-38-187.ap-south-1.compute.internal>
2026-07-05 08:15:08 +0000

    Lifecycle rules added

---
+++ TFLifecycleRule/prevent_before_destroy_ignore_changes.tf
@@ -0,0 +1,24 @@
+resource "terraform_data" "demo" {
+  input = "v2"
+}
+
+resource "aws_instance" "web" {
+
+  ami           = "ami-0f9235932f10668d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web"
+  }
+
+  lifecycle {
+    //prevent_destroy = true
+    create_before_destroy = true
+    ignore_changes = [
+      tags
+    ]
+    replace_triggered_by = [
+      terraform_data.demo
+    ]
+  }
+}
